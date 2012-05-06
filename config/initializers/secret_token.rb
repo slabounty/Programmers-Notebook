29f4ff31c6bdbcf88593005e009c45a55e617938
slabounty <slabounty@gmail.com>
2012-05-05 19:03:30 -0700

    Initial release

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProgrammersNotebook::Application.config.secret_token = '0d4ae6bb08807babf28c71706bbb6d61ab7c36f3b9f3f24068132eb0f1bc8220be7adfed1cc1ffc747bfd1710932ce99536594cbfb11cd38c542ad8ba522e0ce'
