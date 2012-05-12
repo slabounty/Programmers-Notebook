0e40a361e164e502942e01d2aaa6bd6bddae7392
slabounty <slabounty@gmail.com>
2012-05-12 11:02:19 -0700

    Finish static pages

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -6,6 +6,12 @@ describe "Pages" do
       visit '/pages/home'
       page.should have_content('Programmers\' Notebook')
     end
+
+    it "should have the title 'Home'" do
+      visit '/pages/home'
+      page.should have_selector('title',
+                        :text => "Programmer's Notebook | Home")
+    end
   end
 
   describe "Contact page" do
@@ -13,11 +19,24 @@ describe "Pages" do
       visit '/pages/contact'
       page.should have_content('Programmers\' Notebook Contact')
     end
+
+    it "should have the title 'Contact'" do
+      visit '/pages/contact'
+      page.should have_selector('title',
+                        :text => "Programmer's Notebook | Contact")
+    end
   end
+
   describe "About page" do
     it "should have the content 'About'" do
       visit '/pages/about'
       page.should have_content('About')
+    end
+
+    it "should have the title 'About'" do
+      visit '/pages/about'
+      page.should have_selector('title',
+                        :text => "Programmer's Notebook | About")
     end
   end
 end
