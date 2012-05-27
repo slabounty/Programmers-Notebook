fc41c93eb0a0691785ae15db08268ca2b85798c5
slabounty <slabounty@gmail.com>
2012-05-26 20:07:19 -0700

    Finish sign up

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ end
 
 group :test do
   gem 'capybara', '1.1.2'
+  gem 'factory_girl_rails', '1.4.0'
 end
 
 group :production do
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Scott LaBounty"
+    email "slabounty@gmail.com"
+    password "password"
+    password_confirmation "password"
+  end
+end
