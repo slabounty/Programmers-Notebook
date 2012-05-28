b94ac6f4bb28c2eb2ded6d5406940d942124e481
slabounty <slabounty@gmail.com>
2012-05-27 18:38:57 -0700

    Finished sign in.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 ProgrammersNotebook::Application.routes.draw do
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'pages#home'
 
   match '/signup',  to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/help', to: 'pages#help'
   match '/contact', to: 'pages#contact'
